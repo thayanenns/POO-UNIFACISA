231c7288e6926b3400b53da2eff5e56b77c45c17
thayanenns <40129969+thayanenns@users.noreply.github.com>
2020-04-12 01:27:12 -0300

    Questão 1

---
+++ classes-e-objetos/Carro.java
@@ -0,0 +1,97 @@
+package q1Carros;
+
+public class Carro {
+	protected String nome, marca, cor;
+	protected int portas;
+	protected boolean vidroEletrico, arCondicionado, cambioAutomatico, direcaoEletrica;
+	protected double precoBase;
+	
+	public Carro(String nome, String marca, double precoBase ) 
+	{
+		this.nome = nome;
+		this.marca = marca;
+		this.precoBase = precoBase;
+		this.cor = "Branco";
+		this.portas = 2;
+		this.vidroEletrico = false;
+		this.arCondicionado = false;
+		this.cambioAutomatico = false;
+		this.direcaoEletrica = false;
+		
+	}
+	
+	public Carro(String nome, String marca, String cor, double precoBase,int portas, boolean vidroEletrico, boolean arCondicionado, boolean direcaoEletrica, boolean cambioAutomatico) 
+	{
+		this.nome = nome;
+		this.marca = marca;
+		this.cor = cor;
+		this.precoBase = precoBase;
+		this.portas = portas;
+		this.vidroEletrico = vidroEletrico;
+		this.arCondicionado = arCondicionado;
+		this.cambioAutomatico = cambioAutomatico;
+		this.direcaoEletrica = direcaoEletrica;
+		
+	}
+	
+	public Carro() 
+	{
+		
+	}
+	public double calculaPreco() 
+	{
+		double precoTotal = precoBase;
+		switch (cor) {
+			case "branco":
+				break;
+			case "preto":
+				break;
+			case "azul":
+				break;
+			case "vermelho":
+				break;
+				
+			default:
+				precoTotal += 1000;
+		}
+		
+		if (vidroEletrico == true)
+		{
+			precoTotal += 1250;
+		}
+		
+		if (arCondicionado == true) 
+		{
+			precoTotal += 1250;
+		}
+		
+		if(cambioAutomatico == true) 
+		{
+			precoTotal += 1250;
+		}
+		
+		if(direcaoEletrica == true) 
+		{
+			precoTotal += 1250;
+		}
+				
+		return precoTotal;
+	}
+
+	
+	@Override
+	public String toString() {
+		String output = "Nome: " + this.nome + "\n";
+		output += "Marca: " + this.marca + "\n";
+		output += "Cor: " + this.cor + "\n";
+		output += "Preço Base: " + "R$ "+this.precoBase +"\n";
+		output += "Portas: "+ this.portas + "\n";
+		output += "Vidro Elétrico: " +(this.vidroEletrico == true? "Sim" : "Não") + "\n";
+		output += "Ar Condicionado: " +(this.arCondicionado == true? "Sim" : "Não") + "\n";
+		output += "Direção Elétrica: " +(this.direcaoEletrica == true? "Sim" : "Não") + "\n";
+		output += "Câmbio Automático: " +(this.vidroEletrico == true? "Sim" : "Não") + "\n";
+		output += "Preço Total: " +"R$ "+ calculaPreco();
+		
+		return output;
+	}
+}
---
+++ classes-e-objetos/LojaDeCarros.java
@@ -0,0 +1,41 @@
+package q1Carros;
+
+public class LojaDeCarros {
+	public static void main (String [] args) 
+	{
+		Carro palio = new Carro("Palio","Fiat",35000);
+		System.out.println(palio + "\n");
+		
+		Carro palio2 = new Carro("Palio","Fiat",35000);
+		palio2.cor = "Prateado";
+		System.out.println(palio2 + "\n");
+		
+		Carro civic = new Carro("Civic","Honda","Azul",110000,4,true,true,true,true);
+		System.out.println(civic +"\n");
+		
+		Carro corolla = new Carro("Corolla","Toyota",110000);
+		corolla.vidroEletrico = true;
+		corolla.direcaoEletrica = true;
+		corolla.arCondicionado = true;
+		corolla.cambioAutomatico = true;
+		
+		System.out.println(corolla + "\n");
+		
+		Carro gol = new Carro();
+		
+		gol.nome = "Gol";
+		gol.marca = "Volkswagen";
+		gol.cor = "Branco";
+		gol.precoBase = 55000;
+		gol.portas = 4;
+		gol.arCondicionado = true;
+		gol.cambioAutomatico = true;
+		gol.direcaoEletrica = true;
+		gol.vidroEletrico = true;
+		System.out.println(gol);
+		
+	
+		
+		
+	}
+}
