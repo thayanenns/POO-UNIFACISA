684196cb04842d4f8d608c4789cb23e8f4b7ad64
thayanenns <40129969+thayanenns@users.noreply.github.com>
2020-06-07 16:40:41 -0300

    questão 1

---
+++ Polimorfismo/q1/Main.java
@@ -0,0 +1,12 @@
+package zoologico;
+
+public class Main {
+ public static void main(String[] args) {
+	Zoologico z = new Zoologico(); 
+	z.cutucarAnimais();
+	System.out.println(z.escala("cachorro"));
+	
+	
+
+}
+}
